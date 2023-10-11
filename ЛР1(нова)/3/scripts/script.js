136b21ee1b9830d39ab18df7cd21a47bcf286ef6
YuriyIvaniuk <144905718+YuriyIvaniuk@users.noreply.github.com>
2023-10-11 15:26:05 +0300

    Add files via upload

--- ЛР1(нова)/3/scripts/script.js
+++ ЛР1(нова)/3/scripts/script.js
@@ -1,24 +1,32 @@
-function showDialog(){
-var InputValue=document.getElementById("field")
-var monthNumber = InputValue.value
-var Seasons=["Зима", "Весна", "Літо", "Осінь"]
-if (monthNumber==3 || monthNumber==4 || monthNumber==5){
-    alert(Seasons[1]);
-}
-else if(monthNumber==6 || monthNumber==7 || monthNumber==8){
-    alert(Seasons[2]);
-}
-else if(monthNumber==9 || monthNumber==10 || monthNumber==11){
-    alert(Seasons[3]);
-}
-else if(monthNumber==12 || monthNumber==2 || monthNumber==1){
-    alert(Seasons[0]);
-}
-else{
-    alert("Неправильне число");
-}
-}
-
-var showDialogButton = document.getElementById("showDialog");
-showDialogButton.addEventListener("click", showDialog);
-
+function showDialog() {
+    var InputValue = document.getElementById("field");
+    var monthNumber = InputValue.value;
+  
+    if (/^[1-9]|1[0-2]$/.test(monthNumber)) {
+      monthNumber = parseInt(monthNumber);
+      console.log(monthNumber);
+  
+      switch (true) {
+        case monthNumber >= 3 && monthNumber <= 5:
+          alert(`Пора року - Весна`);
+          break;
+        case monthNumber >= 6 && monthNumber <= 8:
+          alert(`Пора року - Літо`);
+          break;
+        case monthNumber >= 9 && monthNumber <= 11:
+          alert(`Пора року - Осінь`);
+          break;
+        case monthNumber <= 2 || monthNumber == 12:
+          alert(`Пора року - Зима`);
+          break;
+        default:
+          alert(`Введіть номер місяця від 1 до 12`);
+      }
+    } else {
+      alert("Некоректний номер місяця. Введіть число від 1 до 12.");
+    }
+  }
+  
+  var showDialogButton = document.getElementById("showDialog");
+  showDialogButton.addEventListener("click", showDialog);
+  
