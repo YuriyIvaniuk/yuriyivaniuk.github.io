e66fdc5e5ed6174f1f28a2255c2495d29bd621d4
YuriyIvaniuk <144905718+YuriyIvaniuk@users.noreply.github.com>
2023-10-23 21:45:59 +0300

    Add files via upload

---
+++ PR1/script/script.js
@@ -0,0 +1,48 @@
+const iconWrap = document.getElementById('iconWrap')
+const iconOpen = document.getElementById('iconOpen')
+const iconClose = document.getElementById('iconClose')
+const mainMenu = document.getElementById('mainMenu')
+
+iconWrap.addEventListener('click',() => {
+	mainMenu.classList.toggle('hide-menu')
+	iconOpen.classList.toggle('hide')
+	iconClose.classList.toggle('hide')
+})
+const aboutButton=document.getElementById("aboutButton")
+const aboutPoint=document.getElementById("aboutPoint");
+aboutButton.addEventListener("click", function(){
+aboutPoint.scrollIntoView({behavior:"smooth"});
+}) 
+const skillsButton=document.getElementById("skillsButton")
+const skillsPoint=document.getElementById("skillsPoint");
+skillsButton.addEventListener("click", function(){
+skillsPoint.scrollIntoView({behavior:"smooth"});
+})
+const portfolioButton=document.getElementById("portfolioButton")
+const latestButton=document.getElementById("latestButton");
+const portfolioPoint=document.getElementById("portfolioPoint");
+portfolioButton.addEventListener("click", function(){
+portfolioPoint.scrollIntoView({behavior:"smooth"});
+})
+latestButton.addEventListener("click", function(){
+portfolioPoint.scrollIntoView({behavior:"smooth"});
+})
+const getInButton=document.getElementById("get-inButton")
+const getInPoint=document.getElementById("get-inPoint");
+getInButton.addEventListener("click", function(){
+getInPoint.scrollIntoView({behavior:"smooth"});
+})
+const topButton=document.getElementById("topButton")
+const topPoint=document.getElementById("topPoint")
+topButton.addEventListener("click", function(){
+topPoint.scrollIntoView({behavior:"smooth"});
+})
+
+function myFunction() {
+	var x = document.getElementById("myLinks");
+	if (x.style.display === "block") {
+	  x.style.display = "none";
+	} else {
+	  x.style.display = "block";
+	}
+}
